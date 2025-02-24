d40a000b6ac721fbb6ba4b30d19527f80aedde9c
Stefanospedicato <stefano.spedicato11@gmail.com>
2025-02-24 17:18:01 +0100

    feat: add home bg

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { GlobalProvider } from "./Contexts/GlobalContext";
 import DefaultLayout from "./Layouts/DefaultLayout";
+import Home from "./Pages/Home";
 
 const App = () => {
   return (
     <GlobalProvider>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<DefaultLayout />}></Route>
+          <Route path="/" element={<DefaultLayout />}>
+            <Route path="/" element={<Home />} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </GlobalProvider>
