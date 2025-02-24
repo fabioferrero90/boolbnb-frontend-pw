601c688819ad92617a9541e954604a1901db6fbd
fabioferrero90 <fabio.ff90@gmail.com>
2025-02-24 17:16:56 +0100

    feat: header + footer

--- src/Layouts/DefaultLayout.jsx
+++ src/Layouts/DefaultLayout.jsx
@@ -2,10 +2,15 @@ import {Outlet} from 'react-router-dom'
 import Header from '../Components/Header'
 import Footer from '../Components/Footer'
 
+const headerMenu = [
+  { route: "/create", name: "Inserisci appartamento", key: "create" },
+  { route: "/account", name: "Il mio account", key: "account" }
+];
+
 const DefaultLayout = () => {
   return (
     <div>
-    <header><Header /></header>
+    <header><Header headerMenu={headerMenu}/></header>
     <main><Outlet /></main>
     <footer><Footer /></footer>
   </div>
