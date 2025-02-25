5140e842abdb6c01a2e24d7ec5586f0ceb4b1ab6
fabioferrero90 <fabio.ff90@gmail.com>
2025-02-25 10:06:36 +0100

    fix double hover

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -31,7 +31,7 @@ const Header = ( {headerMenu} ) => {
                 </li>
               </ul>
             </div>
-            <button data-collapse-toggle="navbar-user" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 text-gray-400 hover:bg-gray-700 focus:ring-gray-600" aria-controls="navbar-user" aria-expanded="false">
+            <button data-collapse-toggle="navbar-user" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm rounded-lg md:hidden focus:outline-none focus:ring-2 text-gray-400 hover:bg-gray-700 focus:ring-gray-600" aria-controls="navbar-user" aria-expanded="false">
               <span className="sr-only">Apri Menu Principale</span>
               <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
                   <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15"/>
