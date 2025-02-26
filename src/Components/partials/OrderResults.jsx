06dee574c4766f2b93ac9fb26f12513aa2ed924a
Stefanospedicato <stefano.spedicato11@gmail.com>
2025-02-26 11:47:20 +0100

    feat: update OrderResults partial

--- src/Components/partials/OrderResults.jsx
+++ src/Components/partials/OrderResults.jsx
@@ -1,24 +1,25 @@
-import { useGlobalContext } from '../../Contexts/GlobalContext'
+import { useGlobalContext } from "../../Contexts/GlobalContext";
 
 const OrderResults = () => {
-
   const { orderedBy, handleOrderByChange } = useGlobalContext();
 
   return (
     <div className="grow">
-      <select 
-        id="countries" 
+      <select
+        id="countries"
         className="bg-gray-200 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
         value={orderedBy}
         onChange={handleOrderByChange}
       >
-        <option value="" disabled>Ordina Per:</option>
+        <option value="" disabled>
+          Ordina Per:
+        </option>
         <option value="price-asc">Prezzo Crescente</option>
         <option value="price-desc">Prezzo Decrescente</option>
-        <option value="most-liked">Like Ricevuti</option>
+        <option value="most-liked">Popolarità</option>
       </select>
     </div>
-  )
-}
+  );
+};
 
-export default OrderResults
+export default OrderResults;
