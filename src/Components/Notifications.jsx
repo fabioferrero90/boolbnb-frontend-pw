b99c4cbb436e54e810279597cbd3b9d49ad128bf
fabioferrero90 <fabio.ff90@gmail.com>
2025-03-13 08:54:50 +0100

    correction overflow for notify

--- src/Components/Notifications.jsx
+++ src/Components/Notifications.jsx
@@ -23,7 +23,7 @@ const Notifications = () => {
 
   return (
     <div className="relative justify-center items-center">
-      <div className="w-[100vw] h-[100vh] absolute">
+      <div className="w-[100vw] h-[100vh] absolute overflow-hidden">
         <div className={`fixed flex flex-col justify-center bottom-10 right-10 custom-bg-color-primary text-white rounded-2xl px-8 py-2 shadow-2xl transition-all z-999 ${animationClass}`}>
           <div className={`flex items-center pt-4 mb-[-1rem] pl-1 ${notificationData.type === "success" ? 'custom-teal-color' : 'text-red-400'}`}>
             {notificationData.type === "success" ? <FaCheckCircle /> : <MdError />}
